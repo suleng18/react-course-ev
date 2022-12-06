f7f45f7f30ad410bc323d37725a469af88946932
Suleng18 <90331457+suleng18@users.noreply.github.com>
2022-12-06 17:48:13 +0700

    form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import './App.scss';
-import SidebarMenu from './components/SidebarMenu';
-import useClickOutSide from './hooks/useClickOutSide';
+import Form from './components/form/Form';
 
 function App() {
-  const { show, setShow, nodeRef } = useClickOutSide('span');
-
   return (
-    <div className="p-5">
-      <span
-        onClick={() => setShow(true)}
-        className="inline-block m-3 p-3 rounded-lg text-white bg-green-400 cursor-pointer"
-      >
-        Show Menu
-      </span>
-      <SidebarMenu show={show} ref={nodeRef}></SidebarMenu>
+    <div>
+      <Form></Form>
     </div>
   );
 }
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,104 +1,84 @@
-import React, { useState } from "react";
-import useHandleChange from "../../hooks/useHandleChange";
+import React from 'react';
+import { useState } from 'react';
 
 const Form = () => {
-  // const [fullname, setFullName] = useState("");
-  // const [message, setMessage] = useState("");
-  // const [country, setCountry] = useState("");
-  // const handleInputChange = (event) => {
-  //   console.log(event.target.value);
-  //   setFullName(event.target.value);
-  // };
-  // const handleTextareaChange = (event) => {
-  //   setMessage(event.target.value);
-  // };
-  // const handleSelectChange = (event) => {
-  //   setCountry(event.target.value);
-  // };
-  // const [values, setValues] = useState({
-  //   fullname: "",
-  //   email: "",
-  //   hobby: false,
-  // });
-  // obj.property : dot notation
-  // obj[property]
-  // console.log("Form ~ values", values);
+  // const [fullname, setFullname] = useState('');
+  // const [message, setMessage] = useState('');
+  // const [country, setCountry] = useState('');
+
   // const handleInputChange = (e) => {
-  //   const type = e.target.type;
-  //   setValues({
-  //     ...values,
-  //     [e.target.name]: type === "checkbox" ? e.target.checked : e.target.value,
-  //   });
-  // if (type === "checkbox") {
-  //   setValues({
-  //     ...values,
-  //     [e.target.name]: e.target.checked,
-  //   });
-  // } else {
-  //   setValues({
-  //     ...values,
-  //     [e.target.name]: e.target.value,
-  //   });
-  // }
+  //   setFullname(e.target.value);
   // };
-  const { values, handleChange } = useHandleChange({
-    fullname: "",
-    email: "",
+
+  // const handleTextareaChange = (e) => {
+  //   setMessage(e.target.value);
+  // };
+
+  // const handleSelectOnChange = (e) => {
+  //   setCountry(e.target.value);
+  // };
+
+  const [values, setValues] = useState({
+    fullname: '',
+    email: '',
     hobby: false,
   });
-  const [nameError, setNameError] = useState("");
-  const handleSubmitForm = (e) => {
-    e.preventDefault();
-    if (values.fullname === "") {
-      setNameError("Your fullname is empty");
-    } else {
-      setNameError("");
-    }
+
+  const handleInputChange = (e) => {
+    const type = e.target.type;
+
+    setValues({
+      ...values,
+      [e.target.name]: type === 'checkbox' ? e.target.checked : e.target.value,
+    });
+
+    // if (type === 'checkbox') {
+    //   setValues({
+    //     ...values,
+    //     [e.target.name]: e.target.checked,
+    //   });
+    // } else {
+    //   setValues({
+    //     ...values,
+    //     [e.target.name]: e.target.value,
+    //   });
+    // }
   };
-  // console.log("Form ~ values", values);
-  // Formik + Yup vs React hook form
-  // regex
+
   return (
     <div className="p-5">
-      <form
-        className="flex gap-x-3"
-        autoComplete="off"
-        onSubmit={handleSubmitForm}
-      >
-        <div className="flex flex-col gap-y-3">
-          <input
-            type="text"
-            name="fullname"
-            className="w-full max-w-[300px] p-5 border border-gray-200 rounded-lg"
-            placeholder="Enter your name"
-            onChange={handleChange}
-          />
-          {nameError}
-        </div>
+      <div className="flex gap-x-3">
+        <input
+          type="text"
+          name="fullname"
+          onChange={handleInputChange}
+          className="w-full max-x-[300px] p-5 border border-gray-200 rounded-lg focus:border-blue-400"
+          placeholder="enter your name"
+        />
         <input
           type="email"
           name="email"
-          className="w-full max-w-[300px] p-5 border border-gray-200 rounded-lg"
-          placeholder="Enter your email address"
-          onChange={handleChange}
+          onChange={handleInputChange}
+          className="w-full max-x-[300px] p-5 border border-gray-200 rounded-lg focus:border-blue-400"
+          placeholder="enter your email address"
         />
-        {/* <input type="checkbox" name="hobby" onChange={handleChange} /> */}
-        <button type="submit" className="p-3 rounded-lg text-white bg-blue-500">
-          Submit
-        </button>
-      </form>
-      {/* {message}
+
+        <input type="checkbox" name="hobby" onChange={handleInputChange} />
+      </div>
+
+      {/* <h3>Message: {message}</h3>
       <textarea
-        className="w-full max-w-[300px] p-5 border border-gray-200 rounded-lg"
-        placeholder="Enter your message"
+        className="w-full max-x-[300px] p-5 border border-gray-200 rounded-lg focus:border-blue-400"
+        placeholder="enter your message"
         name="message"
         onChange={handleTextareaChange}
       ></textarea>
-      {country}
-      <select name="country" onChange={handleSelectChange}>
-        <option value="vietnam">VN</option>
-        <option value="usa">USA</option>
-        <option value="japan">Japan</option>
+
+      <h3>Select: {country}</h3>
+      <select name="country" onChange={handleSelectOnChange}>
+        <option value="vietnam">VietName</option>
+        <option value="USA">America</option>
+        <option value="Korea">Korea</option>
       </select> */}
     </div>
   );
